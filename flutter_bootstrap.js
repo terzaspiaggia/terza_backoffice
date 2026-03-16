903643373d53ee770a0850ecb736326177f2727d
antoniu <tonylaurnic@gmail.com>
2026-03-16 21:57:08 +0200

    last fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,4 +35,8 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7bf","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "3191172053" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  }
+});
